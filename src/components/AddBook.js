f62c9bc09a910de16fdbcc6df78c5fc9594e6c4e
Daniel Corcoran <danielc.92@outlook.com>
2019-08-06 11:05:45 +1000

    Added form

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -15,7 +15,24 @@ export default class AddBook extends Component {
     render() {
         return (
             <div>
-                
+                <form id="add-book">
+                    <div className="field">
+                        <label className="label">Book Name:</label>
+                        <input type="text" className="input"/>
+                    </div>
+
+                    <div className="field">
+                        <label className="label">Book Genre:</label>
+                        <input type="text" className="input"/>
+                    </div>
+
+                    <div className="field">
+                        <label className="label">Book Name:</label>
+                        <select>
+                            <option>Select Author</option>
+                        </select>
+                    </div>
+                </form>
             </div>
         )
     }
